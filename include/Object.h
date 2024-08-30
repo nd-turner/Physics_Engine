5013ce0b70a04f30678810017b5e05943d81cec7
Alicen242 <abarowsky@mail.bradley.edu>
2024-08-30 11:05:27 -0500

    build out more of the object class

--- include/Object.h
+++ include/Object.h
@@ -5,15 +5,40 @@
 
 /*what is an object an object is essentially going to be a defined amount of vertex's that will be joined together and liked into some sort of object*/
 class Object{
+
     private:
-         
+        std::vector<Vertex> vertices;  // Store vertices for the object
+        std::vector<int> elements;
+
+        float Pos[3];   //x y z will get passed to vertices for rendering
+        float Vel[2];   //dx and dy
+        int color[3];   //rgb values that will get passed to color the object
+
+        int resolution;     // Resolution for generating mesh gen
     public:
         
         Object();
         
-        //2d geometry stuff
+        //2d mesh geometry stuff
         std::vector<Vertex> generateCircleMesh(float radius, float pos[3], int resolution);
         std::vector<int> generateCircleElem(std::vector<Vertex> GeoMesh);
+
+        //Logic
+        void updatePosition();
+
+        //collision handling
+        void handleWallCollision();
+        bool isColliding(const Object& other) const;
+        void handleObjectCollision();
+
+        //return functions
+        const float* getPosition() const;
+        const float* getVelocity() const;
+
+
+        
+
+
 
 
 };
